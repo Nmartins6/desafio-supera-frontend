9fae2e8907b843e1ddd74c0a7ae33e55ce60b6cd
Nicolas Martins <nicolas.edm7@gmail.com>
2023-07-17 20:42:49 -0300

    feat: criando estilização, responsividade e integrando frontend e backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [transferencias, setTransferencias] = useState([]);
+  const [dataInicio, setDataInicio] = useState(null);
+  const [dataFim, setDataFim] = useState(null);
+  const [nomeOperador, setNomeOperador] = useState(null);
+
+  useEffect(() => {
+    const params = [];
+
+    if (transferencias.fetching) {
+      return;
+    }
+
+    if (dataInicio !== null) {
+      params.push(`dataInicio=${dataInicio}`);
+    }
+
+    if (dataFim !== null) {
+      params.push(`dataFim=${dataFim}`);
+    }
+
+    if (nomeOperador !== null && nomeOperador.trim() !== "") {
+      params.push(`nomeOperador=${nomeOperador}`);
+    }
+
+    const url = `http://localhost:8080/transferencias?${params.join("&")}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((transferencias) => {
+        setTransferencias(transferencias);
+      });
+  }, [dataInicio, dataFim, nomeOperador]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Desafio Supera Java-React</h1>
+      <form>
+        <div className="SearchFields">
+          <label>
+            <span>Data de início</span>
+            <input
+              type="date"
+              onChange={(e) => setDataInicio(e.target.value)}
+            ></input>
+          </label>
+
+          <label>
+            <span>Data de fim</span>
+            <input
+              type="date"
+              onChange={(e) => setDataFim(e.target.value)}
+            ></input>
+          </label>
+
+          <label>
+            <span>Nome do operador transacionado</span>
+            <input
+              type="text"
+              onChange={(e) => setNomeOperador(e.target.value)}
+            ></input>
+          </label>
+        </div>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Data</th>
+            <th>Valor</th>
+            <th>Tipo</th>
+            <th>Nome do operador transacionado</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transferencias.map((t) => {
+            const dataFormatada = new Date(t.data).toLocaleDateString('pt-BR');
+
+            const valorNumerico = Number(t.valor);
+            const valorFormatado = valorNumerico.toLocaleString('pt-BR', {minimumFractionDigits: 2});
+            return (
+              <tr key={t.id}>
+                <td>{dataFormatada}</td>
+                <td>{valorFormatado}</td>
+                <td>{t.tipo}</td>
+                <td>{t.nomeOperador}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
